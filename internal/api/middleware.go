6c39a6fb47d153bad29722e441de0ef5ea6259ca
IsaqueB <isaquebz.dev@gmail.com>
2026-09-14 17:02:59 -0300

    Revert "recover body after reading it in middleware"
    This reverts commit d6ed2fa4bb421487b8ccac0da5fe4fb0310bf82d.

--- internal/api/middleware.go
+++ internal/api/middleware.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"bytes"
 	"context"
 	"encoding/base64"
 	"encoding/hex"
@@ -91,14 +90,11 @@ func (h *Handler) webhookBlingAuthorization(next http.Handler) http.Handler {
 			return
 		}
 		message, err := io.ReadAll(r.Body)
-		defer r.Body.Close()
 		if err != nil {
 			h.log.Error("webhook middleware auth", "Could not find payload to encode")
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
-		r.Body = io.NopCloser(bytes.NewBuffer(message))
-
 		secret := os.Getenv("BLING_CLIENT_SECRET")
 		defer func() { secret = "" }()
 		if secret == "" {
@@ -128,14 +124,11 @@ func (h *Handler) sheetsAuthorization(next http.Handler) http.Handler {
 			return
 		}
 		message, err := io.ReadAll(r.Body)
-		defer r.Body.Close()
 		if err != nil {
 			h.log.Error("webhook middleware auth", "Could not find payload to encode")
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
-		r.Body = io.NopCloser(bytes.NewBuffer(message))
-
 		secret := os.Getenv("SHEETS_CLIENT_SECRET")
 		defer func() { secret = "" }()
 		if secret == "" {
--- main.go
+++ main.go
@@ -1,26 +1,15 @@
 package main
 
 import (
-	"bytes"
-	"context"
-	"encoding/hex"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"time"
 
 	"github.com/IsaqueB/notification-server/cmd/api"
 	"github.com/IsaqueB/notification-server/cmd/worker"
-	"github.com/IsaqueB/notification-server/internal/auth"
-	"github.com/IsaqueB/notification-server/internal/models"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	runFlag := flag.String("r", "req", "select service to run [api]|[worker] default:[worker]")
+	runFlag := flag.String("r", "worker", "select service to run [api]|[worker] default:[worker]")
 	flag.Parse()
 
 	switch *runFlag {
@@ -28,100 +17,7 @@ func main() {
 		api.Run()
 	case "worker":
 		worker.Run()
-	case "req":
-		aaa()
 	default:
 		fmt.Println("Invalid service")
 	}
-}
-
-func aaa() {
-	// event := models.BlingWebhookEvent[models.BlingWebhookPayloadInvoice]{
-	// 	EventId:   "0",
-	// 	Event:     "invoice.issued",
-	// 	CompanyId: "0",
-	// 	Data: models.BlingWebhookPayloadInvoice{
-	// 		Id:            0,
-	// 		Type:          models.Issued,
-	// 		Situation:     models.Incoming,
-	// 		Number:        "0",
-	// 		EmissionDate:  "2024-09-27 11:24:56",
-	// 		OperationDate: "2024-09-27 11:24:56",
-	// 		Contact: models.BlingWebhookPayloadInvoiceRelatedField{
-	// 			Id: 0,
-	// 		},
-	// 		OperationNature: models.BlingWebhookPayloadInvoiceRelatedField{
-	// 			Id: 0,
-	// 		},
-	// 		Store: models.BlingWebhookPayloadInvoiceRelatedField{
-	// 			Id: 0,
-	// 		},
-	// 	},
-	// }
-	a := `{
-		"eventId": "01945027-150e-72b4-e7cf-4943a042cd9c",
-		"date": "2025-01-10T12:18:46Z",
-		"version": "v1",
-		"event": "invoice.updated",
-		"companyId": "d4475854366a36c86a37e792f9634a51",
-		"data": {
-			"id": 12345678,
-			"tipo": 1,
-			"situacao": 1,
-			"numero": "1234",
-			"dataEmissao": "2024-09-27 11:24:56",
-			"dataOperacao": "2024-09-27 11:00:00",
-			"contato": {
-				"id": 12345678
-			},
-			"naturezaOperacao": {
-				"id": 12345678
-			},
-			"loja": {
-				"id": 12345678
-			}
-		}
-	}`
-	event := &models.BlingWebhookEvent[models.BlingWebhookPayloadInvoice]{}
-	err := json.Unmarshal([]byte(a), event)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	message, err := json.Marshal(event)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	event2 := &models.BlingWebhookEvent[models.BlingWebhookPayloadInvoice]{}
-	err = json.Unmarshal(message, event2)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	godotenv.Load()
-	secret := os.Getenv("BLING_CLIENT_SECRET")
-	secretB, _ := hex.DecodeString(secret)
-	if err != nil {
-		fmt.Println(err)
-	}
-	signature := hex.EncodeToString(auth.Sign_HS256(message, secretB))
-
-	client := &http.Client{
-		Timeout: 30 * time.Second,
-	}
-	req, err := http.NewRequestWithContext(
-		context.Background(),
-		"POST",
-		"https://app-notify.bracomil.com.br/api/webhook/bling/invoice",
-		bytes.NewBuffer(message),
-	)
-	if err != nil {
-		fmt.Println(err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("X-Bling-Signature-256", signature)
-	res, err := client.Do(req)
-	b, _ := io.ReadAll(res.Body)
-	fmt.Println(b)
 }
